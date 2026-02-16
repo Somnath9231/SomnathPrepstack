71be7af85e0288230eeff0123a48dc9e3020a733
SOMNATH Podder <poddersomnath598@gmail.com>
2026-02-16 18:45:23 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9149725077-8d5e9",
+  "appId": "1:205718975637:web:8ecd559817c405b482d79c",
+  "apiKey": "AIzaSyBKqwQ_2juIhCikkoyOejmd48Pe2-bVs_M",
+  "authDomain": "studio-9149725077-8d5e9.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "205718975637"
+};
